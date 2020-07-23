e94d64b05ad09174197def2030ee77c497bcb241
dudov-al <31045292+dudov-al@users.noreply.github.com>
2020-07-23 22:26:04 +0300

    Add files via upload

---
+++ precache-manifest.0eeeaf5b07a3ee8cdf6ea791e443c5da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4344fb8da7bd1e4647c740977b86708b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f343346874bd2b478f4c",
+    "url": "/static/js/2.7a29c770.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a29c770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd7665b9bc2f01be597",
+    "url": "/static/js/main.591c11a6.chunk.js"
+  },
+  {
+    "revision": "0e90619b1d5b396aa995",
+    "url": "/static/js/runtime-main.1065a662.js"
+  }
+]);
